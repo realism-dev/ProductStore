43a871ed2b42f3a39fb75b7dd0320757f349768f
Realism <88169606+GovardRork@users.noreply.github.com>
2025-05-02 14:19:05 +0300

    1. updated gitignore 2. viewmodel refactored

--- app/src/main/kotlin/dev/realism/productstore/productlistscreen/presentation/ProductListScreenViewModel.kt
+++ app/src/main/kotlin/dev/realism/productstore/productlistscreen/presentation/ProductListScreenViewModel.kt
@@ -2,23 +2,20 @@ package dev.realism.productstore.productlistscreen.presentation
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import dev.realism.productstore.core.data.source.local.LocalDataSource
 import dev.realism.productstore.core.domain.model.ProductItem
 import dev.realism.productstore.productlistscreen.domain.usecase.DeleteProductItemUseCase
 import dev.realism.productstore.productlistscreen.domain.usecase.GetAllProductItemsUseCase
 import dev.realism.productstore.productlistscreen.domain.usecase.UpdateProductItemUseCase
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class ProductListScreenViewModel @Inject constructor(
-    private val delete:DeleteProductItemUseCase,
-    private val getAll:GetAllProductItemsUseCase,
-    private val update:UpdateProductItemUseCase
-) :
-    ViewModel(), LocalDataSource {
+    private val delete: DeleteProductItemUseCase,
+    private val getAll: GetAllProductItemsUseCase,
+    private val update: UpdateProductItemUseCase
+) : ViewModel() {
     private var _productList = MutableStateFlow<List<ProductItem>>(emptyList())
     val productList: StateFlow<List<ProductItem>> = _productList
 
@@ -28,21 +25,17 @@ class ProductListScreenViewModel @Inject constructor(
 
     fun updateProductList(searchQuery: String = "") {
         viewModelScope.launch {
-            getAllProductItems(searchQuery).collect {
+            getAll(searchQuery).collect {
                 _productList.value = it
             }
         }
     }
 
-    override suspend fun getAllProductItems(searchQuery: String): Flow<List<ProductItem>> {
-        return getAll(searchQuery)
-    }
-
-    override suspend fun updateProductItem(productItem: ProductItem) {
+    suspend fun updateProductItem(productItem: ProductItem) {
         update(productItem)
     }
 
-    override suspend fun deleteProductItem(productItem: ProductItem) {
+    suspend fun deleteProductItem(productItem: ProductItem) {
         delete(productItem)
     }
 }
